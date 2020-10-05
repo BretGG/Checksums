45677788f69719efd519780463cf8419fa5953f5
Mikhaela Layon <mapl.layon@gmail.com>
2020-10-05 13:09:50 -0700

    Written encode/decode checksum programs without parity

--- decodeChecksum.c
+++ decodeChecksum.c
@@ -14,7 +14,7 @@ int main(int argc, char* argv[]) {
     }
 
     if (argc > 2) {
-        status = initialize(argv[2], &block);
+        status = initialize(argv[1], &block);
     }
     else {
         status = initialize("", &block);
@@ -26,22 +26,49 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    // Reading all data from input*/
+    // Reading all data from input
+    int sum = 0; // used to store result from XOR addition
     while (getBlock(block, 1) == SUCCESS && block->byteCount > 0) {
-        int bit = 0;
+        int bit = 0; // is this where current 8-bit block will be stored?
         for (int i = 0; i < 8; i++){
             bit ^= getBit(block->data, i);
         }
 
-        if (strcmp(argv[1], "--odd") == 0)
-            bit ^= 1;
+        // Addition using XOR
+        // src: https://www.geeksforgeeks.org/add-two-numbers-without-using-arithmetic-operators/
+        while (bit != 0) {
 
-        writeBlock(block);
+            // Getting carry by getting common bits
+            // from prev and current block
+            int carry = sum & bit;
 
-        getBlock(block, 1);
-        if (block->data != bit)
-            writeErrorBlock(block, "parity checksum failed");
+            // Sum of prev and current block
+            sum ^= bit;
 
+            bit = carry << 1;
+        }
+
+        // TODO: Uncomment if this is indeed the parity check logic
+        // if (strcmp(argv[1], "--odd") == 0)
+        //     bit ^= 1;
+
+        // getBlock(block, 1);
+        // if (block->data != bit)
+        //     writeErrorBlock(block, "parity checksum failed");
+    }
+
+    // Check if result is all 1's
+    // src: https://www.geeksforgeeks.org/count-set-bits-in-an-integer/
+    // Using Brian Kernighan's algorithm
+    int setBits = 0;
+    while (sum) {
+        sum &= (sum - 1);
+        setBits++;
+    }
+
+    // If the sum's bits are not all 1's (or set)
+    if (setBits != 8) {
+        writeErrorBlock(block, "parity checksum failed");
     }
 
     closeBlock(block);
--- encodeChecksum.c
+++ encodeChecksum.c
@@ -26,20 +26,44 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    // Reading all data from input*/
+    // Reading all data from input
+    int sum = 0; // used to store result from XOR addition
     while (getBlock(block, 1) == SUCCESS && block->byteCount > 0) {
-        int bit = 0;
+        int bit = 0; // is this where current 8-bit block will be stored?
         for (int i = 0; i < 8; i++){
             bit ^= getBit(block->data, i);
         }
 
-        if (strcmp(argv[1], "--odd") == 0)
-            bit ^= 1;
+        // Addition using XOR
+        // src: https://www.geeksforgeeks.org/add-two-numbers-without-using-arithmetic-operators/
+        while (bit != 0) {
 
-        writeBlock(block);
-        block->data = bit;
-        writeBlock(block);
+            // Getting carry by getting common bits
+            // from prev and current block
+            int carry = sum & bit;
+
+            // Sum of prev and current block
+            sum ^= bit;
+
+            bit = carry << 1;
+        }
+
+        // TODO: Uncomment if this is indeed the parity check logic
+        // if (strcmp(argv[1], "--odd") == 0)
+        //     bit ^= 1;
+
+        // writeBlock(block);
+        // block->data = bit;
+        // writeBlock(block);
     }
+
+    // Generate checksum by performing one's complement on sum
+    int checksum = ~sum;
+
+    // Append checksum to message
+    // TODO: Might need to have a createBlock function in IO lib?
+    Block checksumBlock = {checksum, 1};
+    writeBlock(checksumBlock);
 
     closeBlock(block);
     (void) argc;
